21bfdf25afb99ff2073711841ffe35c974eb4fa1
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-02-06 13:38:17 -0600

    Update 02_adjustVCF_for_plotting.sh

--- 03_Dog_Analyses/02_adjustVCF_for_plotting.sh
+++ 03_Dog_Analyses/02_adjustVCF_for_plotting.sh
@@ -8,14 +8,24 @@
 #SBATCH --output=/home/lh106/Rice/Dog_Selection/slurmout/plink.out 
 #SBATCH --error=/home/lh106/Rice/Dog_Selection/slurmout/plink.err
 
-VCF=/scratch/lh106/results_chrX/chrX_TKTL1.recode.vcf
-OUTBED=/scratch/lh106/results_chrX/chrX_TKTL1.bed
-OUTPLINK=/scratch/lh106/results_chrX/chrX_TKTL1.plink
-OUTVCF_final=/scratch/lh106/results_chrX/chrX_TKTL1_finalcoded
+OUT=/scratch/yourID/results_chrX/KRT71_vcf.recode.vcf
+OUTBED=/scratch/Dog_Selection/results/KRT71_bed
+OUTPLINK=/scratch/Dog_Selection/results/KRT71_plink
+OUTVCF_final=/scratch/Dog_Selection/results/KRT71_plink_finalcoded
 
 /home/lh106/software/plink --vcf ${VCF} --make-bed --const-fid --dog --out ${OUTBED} #to sort the chromosomes
 /home/lh106/software/plink --bfile ${OUTBED} --recode --out ${OUTPLINK} --const-fid --dog
 /home/lh106/software/plink --file  ${OUTPLINK} --recode vcf --out ${OUTVCF_final} --const-fid --dog
+
+## might also have to download plink 
+cd /home/yourID
+mkdir software
+cd software
+wget https://s3.amazonaws.com/plink1-assets/plink_linux_x86_64_20250819.zip
+unzip plink_linux_x86_64_20250819.zip
+#test
+./plink
+
 
 
 
