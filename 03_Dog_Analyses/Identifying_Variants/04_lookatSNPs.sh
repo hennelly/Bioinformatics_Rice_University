a8e5e318273d920bd9fa6fae4d4548d9482137fa
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-02-06 13:53:36 -0600

    Update 04_lookatSNPs.sh

--- 03_Dog_Analyses/Identifying_Variants/04_lookatSNPs.sh
+++ 03_Dog_Analyses/Identifying_Variants/04_lookatSNPs.sh
@@ -1,19 +1,19 @@
-#!/usr/bin/env bash
-#SBATCH --job-name=intersect
-#SBATCH -c 1
-#SBATCH --time 1-02:10:00
-#SBATCH --mem-per-cpu 50G
-#SBATCH -o /home/crq857/projects/Redwolf/slurmout/intersect.out
-#SBATCH -e /home/crq857/projects/Redwolf/slurmout/intersect.err
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=00:30:00 
+#SBATCH --output=/home/yourname/Rice/Dog_Selection/slurmout/plink.out 
+#SBATCH --error=/home/yourname/Rice/Dog_Selection/slurmout/plink.err
+
+FILE=/scratch/yourID/Dog_Selection/Results/KRT71_filtered_fst_noNAN_results.weir.fst.txt
+OUTFILE=/scratch/yourID/Dog_Selection/Results/KRT71_filtered_fst_noNAN_results_sort.weir.fst.txt
+
+sort -k3,3nr ${OUTFILE} > ${OUTFILE}
+
+wc -l ${$OUTFILE}
 
 
-module load perl
-module load gsl/2.5
-module load bcftools
-
-cd /projects/psg/people/crq857/TSHR_Oct2025/01_Reprogenes_scan/candidate_genes 
-sort -k3,3nr 1715g_WildSled_SNP_INDEL_perSNP_fst_scan_minOct21_2025_minQ30_minDP3_maxmiss0.8_nonan.weir.fst.txt > 1715g_WildSled_SNP_INDEL_perSNP_fst_scan_minOct21_2025_minQ30_minDP3_maxmiss0.8_nonan.weir.fst_sort.txt
-
-wc -l 1715g_WildSled_SNP_INDEL_perSNP_fst_scan_minOct21_2025_minQ30_minDP3_maxmiss0.8_nonan.weir.fst_sort.txt
-#64281937 total sites including the X chromosome
 
