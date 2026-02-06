14c46b0909c8f1cbad97eb8886bf98dcfa244d4f
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-02-06 14:16:30 -0600

    Update 04_looking_at_SNPs.sh

--- 03_Dog_Analyses/Example/Identify_variant/04_looking_at_SNPs.sh
+++ 03_Dog_Analyses/Example/Identify_variant/04_looking_at_SNPs.sh
@@ -9,12 +9,12 @@
 #SBATCH --output=/home/yourname/Rice/Dog_Selection/slurmout/plink.out 
 #SBATCH --error=/home/yourname/Rice/Dog_Selection/slurmout/plink.err
 
-FILE=/scratch/yourID/Dog_Selection/Results/KRT71_filtered_fst_noNAN_results.weir.fst.txt
-OUTFILE=/scratch/yourID/Dog_Selection/Results/KRT71_filtered_fst_noNAN_results_sort.weir.fst.txt
+FILE=/scratch/yourID/Dog_Selection/Results/TKTL1_filtered_nonan.weir.fst.txt
+OUTFILE=/scratch/yourID/Dog_Selection/Results/TKTL1_filtered_nonan.weir_sort.fst.txt
 
 sort -k3,3nr ${OUTFILE} > ${OUTFILE}
 
 wc -l ${$OUTFILE}
 
 ### Move to local computer 
-scp -r lh106@nots.rice.edu:/scratch/yourID/Dog_Selection/Results/KRT71_filtered_fst_noNAN_results_sort.weir.fst.txt ~/Desktop
+scp -r lh106@nots.rice.edu:/scratch/yourID/Dog_Selection/Results/TKTL1_filtered_nonan.weir_sort.fst.txt ~/Desktop
