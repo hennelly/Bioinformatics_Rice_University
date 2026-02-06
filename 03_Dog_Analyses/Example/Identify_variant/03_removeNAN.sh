3fad0c5acbd0ae88ffe6974c65a2a6624312a62c
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-02-06 14:15:26 -0600

    Update 03_removeNAN.sh

--- 03_Dog_Analyses/Example/Identify_variant/03_removeNAN.sh
+++ 03_Dog_Analyses/Example/Identify_variant/03_removeNAN.sh
@@ -8,6 +8,7 @@
 #SBATCH --output=/home/lh106/Rice/Dog_Selection/slurmout/plink.out 
 #SBATCH --error=/home/lh106/Rice/Dog_Selection/slurmout/plink.err
 
-FILE=/scratch/yourID/Dog_Selection/Results/KRT71_filtered_fst_results.weir.fst.txt
-DIR=/scratch/yourID/Dog_Selection/Results/KRT71_filtered_fst_noNAN_results.weir.fst.txt
-sed '/n/d' ${FILE} >  ${DIR}/1715g_WildSled_SNP_INDEL_perSNP_fst_scan_minOct21_2025_minQ30_minDP3_maxmiss0.8_nonan.weir.fst.txt
+FILE=/scratch/yourID/Dog_Selection/Results/TKTL1_filtered_fst_results.weir.fst.txt
+DIR=/scratch/yourID/Dog_Selection/Results/TKTL1_filteredfst_noNAN_results.weir.fst.txt
+
+sed '/n/d' ${FILE} >  ${DIR}/TKTL1_filtered_nonan.weir.fst.txt
