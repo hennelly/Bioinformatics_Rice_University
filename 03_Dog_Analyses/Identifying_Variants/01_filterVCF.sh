949408feee41d68b68c670d349f39ad5b453842c
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-02-06 13:45:24 -0600

    Update 01_filterVCF.sh

--- 03_Dog_Analyses/Identifying_Variants/01_filterVCF.sh
+++ 03_Dog_Analyses/Identifying_Variants/01_filterVCF.sh
@@ -8,22 +8,16 @@
 #SBATCH --output=/home/lh106/Rice/Dog_Selection/slurmout/plink.out 
 #SBATCH --error=/home/lh106/Rice/Dog_Selection/slurmout/plink.err
 
-OUT=/scratch/yourID/results_chrX/KRT71_vcf.recode.vcf
-OUTBED=/scratch/Dog_Selection/results/KRT71_bed
-OUTPLINK=/scratch/Dog_Selection/results/KRT71_plink
-OUTVCF_final=/scratch/Dog_Selection/results/KRT71_plink_finalcoded
+module load GCC/13.3.0
+module load VCFtools/0.1.17
 
-/home/lh106/software/plink --vcf ${VCF} --make-bed --const-fid --dog --out ${OUTBED} #to sort the chromosomes
+VCF=/scratch/yourID/Dog_Selection/Results/KRT71_vcf.recode.vcf
+DIR=/scratch/yourID/Dog_Selection/Results
 
-
-
-
-
-vcftools --gzvcf ${VCF} \
+vcftools --vcf ${VCF} \
 --minQ 30 \
 --minDP 3 \
 --max-missing 0.8 \
---chr ${CHR} \
 --recode \
 --recode-INFO-all \
---out ${DIR}/1715g_WildSled_SNP_INDEL_perSNP_fst_scan_minOct21_2025_minQ30_minDP3_maxmiss0.8_${CHR}
+--out ${DIR}/KRT71_filtered
