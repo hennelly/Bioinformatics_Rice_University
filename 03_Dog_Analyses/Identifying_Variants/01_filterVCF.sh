a2d06e28919faaede7497bebffa6f5c930f4bc9a
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-02-06 13:44:07 -0600

    Update 01_filterVCF.sh

--- 03_Dog_Analyses/Identifying_Variants/01_filterVCF.sh
+++ 03_Dog_Analyses/Identifying_Variants/01_filterVCF.sh
@@ -1,3 +1,19 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=00:30:00 
+#SBATCH --output=/home/lh106/Rice/Dog_Selection/slurmout/plink.out 
+#SBATCH --error=/home/lh106/Rice/Dog_Selection/slurmout/plink.err
+
+OUT=/scratch/yourID/results_chrX/KRT71_vcf.recode.vcf
+OUTBED=/scratch/Dog_Selection/results/KRT71_bed
+OUTPLINK=/scratch/Dog_Selection/results/KRT71_plink
+OUTVCF_final=/scratch/Dog_Selection/results/KRT71_plink_finalcoded
+
+/home/lh106/software/plink --vcf ${VCF} --make-bed --const-fid --dog --out ${OUTBED} #to sort the chromosomes
 
 
 
