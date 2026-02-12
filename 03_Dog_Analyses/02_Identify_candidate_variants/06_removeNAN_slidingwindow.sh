1df86eae384d92ad3707a6bd063a46695a171935
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-02-12 00:05:26 -0500

    Create 06_removeNAN_slidingwindow.sh

---
+++ 03_Dog_Analyses/02_Identify_candidate_variants/06_removeNAN_slidingwindow.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=00:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/removeNAN.out 
+#SBATCH --error=/home/ak308/Bio310/output/removeNAN.err
+
+FILE=/scratch/ak308/Dog_Selection/Results/KRT71_filtered_fst_results.weir.fst
+DIR=/scratch/ak308/Dog_Selection/Results/
+
+sed '/n/d' ${FILE} >  ${DIR}/KRT71_filtered_fst_results_noNAN.weir.fst.txt
