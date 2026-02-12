4dd748bf2e0076b0d8f092aa22cfb31fc47b2106
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-02-11 23:58:12 -0500

    Update 03_removeNAN.sh

--- 03_Dog_Analyses/02_Identify_candidate_variants/03_removeNAN.sh
+++ 03_Dog_Analyses/02_Identify_candidate_variants/03_removeNAN.sh
@@ -8,6 +8,10 @@
 #SBATCH --output=/home/ak308/Bio310/output/removeNAN.out 
 #SBATCH --error=/home/ak308/Bio310/output/removeNAN.err
 
-FILE=
+FILE=/scratch/ak308/Dog_Selection/Results/KRT71_filtered_fst_results.weir.fst
+DIR=/scratch/ak308/Dog_Selection/Results/
 
-sed '/n/d' ${FILE} >  ${DIR}/1715g_WildSled_SNP_INDEL_perSNP_fst_scan_minOct21_2025_minQ30_minDP3_maxmiss0.8_nonan.weir.fst.txt
+sed '/n/d' ${FILE} >  ${DIR}/KRT71_filtered_fst_results_noNAN.weir.fst.txt
+
+
+
