c1b91d668a441cadf86e6bc7a3e66311643e0d4d
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-03-05 15:31:31 -0600

    Create 02_adjust_ancientVCF.sh

---
+++ 03_Dog_Analyses/03_ancient_samples/02_adjust_ancientVCF.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=00:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/plink.out 
+#SBATCH --error=/home/ak308/Bio310/output/plink.err
+
+OUT=/scratch/ak308/Dog_Selection/Results/KRT71_vcf.recode.vcf
+OUTBED=/scratch/ak308/Dog_Selection/Results/KRT71_bed
+OUTPLINK=/scratch/ak308/Dog_Selection/Results/KRT71_plink
+OUTVCF_final=/scratch/ak308/Dog_Selection/Results/KRT71_plink_finalcoded
+
+/home/ak308/Software/plink --vcf ${VCF} --make-bed --const-fid --dog --out ${OUTBED} #to sort the chromosomes
+/home/ak308/Software/plink --bfile ${OUTBED} --recode --out ${OUTPLINK} --const-fid --dog
+/home/ak308/Software/plink --file  ${OUTPLINK} --recode vcf --out ${OUTVCF_final} --const-fid --dog
+
+cd /scratch/ak308/Dog_Selection/Results/
+##### Convert vcf to coding ######
+sed -i 's:0/0:0:g' KRT71_plink_finalcoded.vcf
+sed -i 's:0/1:1:g' KRT71_plink_finalcoded.vcf
+sed -i 's:1/0:1:g' KRT71_plink_finalcoded.vcf
+sed -i 's:1/1:2:g' KRT71_plink_finalcoded.vcf
+sed -i 's:./.:3:g' KRT71_plink_finalcoded.vcf
+
+
+### Move to local computer 
+scp -r ak308@nots.rice.edu:/scratch/ak308/Dog_Selection/Results/KRT71_plink_finalcoded.vcf ~/Desktop
