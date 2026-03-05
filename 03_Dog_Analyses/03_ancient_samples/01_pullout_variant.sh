6a1d3a29471edd555bc085d3bba2f30bde6a3d66
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-03-05 15:30:52 -0600

    Create 01_pullout_variant.sh

---
+++ 03_Dog_Analyses/03_ancient_samples/01_pullout_variant.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+#SBATCH --job-name=combine 
+#SBATCH --account=commons 
+#SBATCH --partition=commons 
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=1000m 
+#SBATCH --time=00:30:00 
+#SBATCH --output=/home/ak308/Bio310/output/vcfselect.out 
+#SBATCH --error=/home/ak308/Bio310/output/vcfselect.err
+
+# Grab the specific gene from the large VCF
+
+module load GCC/13.3.0
+module load VCFtools/0.1.17
+
+VCF=/storage/hpc/work/lh106/SharedData/AncientimputedVCF/merged_phased_annotated.Autosomes_MAF_0.01_recalibrated_INFO_0.8_header.vcf.gz
+BEDIN=/scratch/ak308/bedfiles/KRT71.bed
+OUT=/scratch/ak308/Bio310_Dog/Results/KRT71_vcf_ancientcanids
+
+vcftools --gzvcf ${VCF} --bed  ${BEDIN} --recode --recode-INFO-all --out ${OUT}
+
+#BED = candidate region 
