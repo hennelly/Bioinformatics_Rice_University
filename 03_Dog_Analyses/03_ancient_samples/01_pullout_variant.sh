6b01b95d7358e9a39c46a5383152454d88cd941c
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-03-11 12:53:45 -0500

    Update 01_pullout_variant.sh

--- 03_Dog_Analyses/03_ancient_samples/01_pullout_variant.sh
+++ 03_Dog_Analyses/03_ancient_samples/01_pullout_variant.sh
@@ -13,7 +13,7 @@
 module load GCC/13.3.0
 module load VCFtools/0.1.17
 
-VCF=/scratch/lh106/SharedData//merged_phased_annotated.Autosomes_ancientdogdingo_FINAL_MAF_0.01_recalibrated_INFO_0.8.vcf.gz
+VCF=/scratch/lh106/SharedData/1697canidVCF/g_WildSled.SNP.INDEL.chrAll.newID.vcf.gz
 OUT=/scratch/lh106/Dog_Selection/Fstscan_March52026/longhair_vcf_modern
 
 vcftools --gzvcf ${VCF} --chr chr32 --from-bp 4509367 --to-bp 4509367 --recode --recode-INFO-all --out ${OUT}
