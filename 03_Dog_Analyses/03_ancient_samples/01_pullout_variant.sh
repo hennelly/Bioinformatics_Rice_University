4b498b7e39b0b948504e26f2f23d407faebdf5b2
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-03-11 12:53:26 -0500

    Update 01_pullout_variant.sh

--- 03_Dog_Analyses/03_ancient_samples/01_pullout_variant.sh
+++ 03_Dog_Analyses/03_ancient_samples/01_pullout_variant.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks=1 
 #SBATCH --mem-per-cpu=1000m 
 #SBATCH --time=00:30:00 
-#SBATCH --output=/home/ak308/Bio310/output/vcfselect.out 
-#SBATCH --error=/home/ak308/Bio310/output/vcfselect.err
+#SBATCH --output=/home/lh106/Rice/Dog_Selection/slurmout/vcfselect.out 
+#SBATCH --error=/home/lh106/Rice/Dog_Selection/slurmout/vcfselect.err
 
 # Grab the specific gene from the large VCF
 
@@ -14,8 +14,12 @@ module load GCC/13.3.0
 module load VCFtools/0.1.17
 
 VCF=/scratch/lh106/SharedData//merged_phased_annotated.Autosomes_ancientdogdingo_FINAL_MAF_0.01_recalibrated_INFO_0.8.vcf.gz
-OUT=/scratch/ak308/Bio310_Dog/Results/KRT71_vcf_ancientcanids
+OUT=/scratch/lh106/Dog_Selection/Fstscan_March52026/longhair_vcf_modern
 
-vcftools --gzvcf ${VCF} --chr [chromosome_name] --from-bp [position] --to-bp [position] --recode --recode-INFO-all --out ${OUT}
+vcftools --gzvcf ${VCF} --chr chr32 --from-bp 4509367 --to-bp 4509367 --recode --recode-INFO-all --out ${OUT}
 
-#BED = candidate region 
+
+
+scp -r lh106@nots.rice.edu:/scratch/lh106/Dog_Selection/Fstscan_March52026/longhair_vcf_ancientcanids.recode.vcf ~/Desktop
+
+/scratch/lh106/Dog_Selection/Fstscan_March52026/longhair_vcf_ancientcanids.recode.vcf
