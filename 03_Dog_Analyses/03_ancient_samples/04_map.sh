f8746210bcc3882ef8354dcac04702757a3b7f40
Lauren Hennelly <lmhennelly@ucdavis.edu>
2026-03-24 13:25:12 -0500

    Update 04_map.sh

--- 03_Dog_Analyses/03_ancient_samples/04_map.sh
+++ 03_Dog_Analyses/03_ancient_samples/04_map.sh
@@ -8,19 +8,22 @@ library(mapplots)
 library(ggplot2)
 library(scatterpie)
 
-dat <- read.csv("GDEF9locus_age.csv", header=TRUE)
+dat <- read.csv("Ancient_MGAM_SI_Locus_age_dogs.csv", header=TRUE)
 
 datlupus <- dat[dat$Species == "lupus", ]
 datfamiliaris <- dat[dat$Species == "familiaris", ]
 
+## TIME LINE 
+ggplot(datfamiliaris, aes(x=Long, y=Age_Name_BP, fill=SI)) + geom_point(shape = 21, size = 4, color = "black", stroke = 0.5) +  scale_fill_manual(values = c("gray","mediumseagreen", "turquoise2", "orange2", "white", "orange2"))  
+
+
 ##################################
-## Ancient Dog Paleolithic map ##
+## 11kya to 7kya map ##
 ##################################
 datlupus <- dat[dat$Species == "lupus", ]
 datfamiliaris <- dat[dat$Species == "familiaris", ]
-datPaleolithic <- dat[dat$Paleolithic_dog == "yes", ]
+datSecondset_4kya_10kya <- datfamiliaris[datfamiliaris$Secondset_7kya_10kya == "yes", ]
 
-#GDF9 
 
 world <- map_data('world')
 p <- ggplot(world, aes(lat, long)) +
@@ -28,13 +31,62 @@ p <- ggplot(world, aes(lat, long)) +
     coord_quickmap() + theme_classic() 
 
 q <- p +
-    geom_point(dat = datfamiliaris, aes(x = Long, y = Lat , fill=short_genotype, color = short_genotype), size = 4, stroke = 1)  + scale_fill_manual(values = c("turquoise2","orange2", "turquoise2", "orange2", "white", "orange2")) +  scale_color_manual(values = c("white","orange2", "mediumseagreen", "mediumseagreen", "turquoise2", "orange2"))   + 
-  scale_shape_manual(values=c(21, 23, 22)) 
+    geom_point(dat = datSecondset_4kya_10kya, aes(x = Long, y = Lat , fill=SI), size = 4, stroke = 1, shape=21, color="black", stroke=1)  + scale_fill_manual(values = c("mediumseagreen","orange2", "orange2", "orange2", "white", "orange2"))  
 
-r <-  q+  theme(line = element_blank(),
+q+  theme(line = element_blank(),
         text = element_blank(),
         title = element_blank()) +
         scale_x_continuous(limits = c(-15, 180))+
         scale_y_continuous(limits = c(-50,80)) 
-r
 
+
+ggsave("Aug27_2025_map_ancientdogs_10to4kya.tiff", width=7,height=4) 
+
+##################################
+##  7kya to 4kya ##
+##################################
+
+
+datlupus <- dat[dat$Species == "lupus", ]
+datfamiliaris <- dat[dat$Species == "familiaris", ]
+Secondset_7kya.4kya <- datfamiliaris[datfamiliaris$Secondset_7kya_4kya == "yes", ]
+
+world <- map_data('world')
+p <- ggplot(world, aes(lat, long)) +
+    geom_map(map=world, aes(map_id=region), fill="lightgray", color=NA) +
+    coord_quickmap() + theme_classic() 
+
+q <- p +
+    geom_point(dat = Secondset_7kya.4kya, aes(x = Long, y = Lat , fill=SI), size = 4, stroke = 1, shape=21, color="black")  + scale_fill_manual(values = c("gray","orange2", "turquoise2", "orange2", "white", "orange2"))  
+
+q+  theme(line = element_blank(),
+        text = element_blank(),
+        title = element_blank()) +
+        scale_x_continuous(limits = c(-15, 180))+
+        scale_y_continuous(limits = c(-50,80)) 
+
+
+ggsave("CELL_Sept17_2025_map_ancientdogs_4kya_to_present.tiff", width=7,height=4) 
+
+##################################
+##  4,000 to present years ago ##
+##################################
+
+
+datlupus <- dat[dat$Species == "lupus", ]
+datfamiliaris <- dat[dat$Species == "familiaris", ]
+Secondset_4kya_present <- datfamiliaris[datfamiliaris$Secondset_4kya_present == "yes", ]
+
+world <- map_data('world')
+p <- ggplot(world, aes(lat, long)) +
+    geom_map(map=world, aes(map_id=region), fill="lightgray", color=NA) +
+    coord_quickmap() + theme_classic() 
+
+q <- p +
+    geom_point(dat = Secondset_4kya_present, aes(x = Long, y = Lat , fill=SI), size = 4, stroke = 1, shape=21, color="black")  + scale_fill_manual(values = c("mediumseagreen","turquoise2", "orange2", "orange2", "white", "orange2"))  
+
+q+  theme(line = element_blank(),
+        text = element_blank(),
+        title = element_blank()) +
+        scale_x_continuous(limits = c(-15, 180))+
+        scale_y_continuous(limits = c(-50,80)) 
